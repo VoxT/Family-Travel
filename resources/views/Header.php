b4771b51a53e6e5fde7d8801339022d2e36cc836
caunguyen <caunguyen861994@gmail.com>
2016-09-24 09:56:57 +0700

    day nek

---
+++ resources/views/Header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FAMILY-TRAVEL</title>
+	<link rel="shortcut icon" type="image/x-icon" href="picture/logo.png" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.6-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.6-dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+<body>
+
+
+</body>
