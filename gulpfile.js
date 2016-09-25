1669cea8d11e22646a83a85879dc535af57e5561
Vo Thieu <votienthieu@gmail.com>
2016-09-25 17:36:27 +0700

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ var bowerDir ='vendor/bower/';
 
 elixir(function(mix) {
 	mix .copy( bowerDir + 'bootstrap/less/**', 'resources/assets/less/bootstrap')
-		.copy( bowerDir + 'bootstrap/dist/fonts/**', 'public/fonts')
+		.copy( bowerDir + 'bootstrap/dist/fonts/**', 'public/build/fonts')
 		.copy( bowerDir + 'bootstrap/dist/js/bootstrap.js', 'resources/assets/js')
 		.copy( bowerDir + 'jquery/dist/jquery.js', 'resources/assets/js')
 		.copy( bowerDir + 'font-awesome/less/**', 'resources/assets/less/fontawesome')
-		.copy( bowerDir + 'font-awesome/fonts', 'public/fonts')
+		.copy( bowerDir + 'font-awesome/fonts', 'public/build/fonts')
 		.copy( bowerDir + 'animate.css/animate.css', 'resources/assets/less/animate');
 
 	mix.less('app.less');
