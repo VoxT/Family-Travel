f6d44e2a3e21f7cbc1dd57d0af16d168f030c69d
Vo Thieu <votienthieu@gmail.com>
2016-09-26 10:07:16 +0700

    test travis-ci

---
+++ resources/views/findresult.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FAMILY-TRAVEL</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+	<script src="{{ elixir('js/app.js') }}"></script>
+</head>
+<body>
+	<div class="navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Brand</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li class="active"><a href="#">Máy Bay <span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Khách Sạn</a></li>
+	        <li><a href="#">Thuê xe</a></li>
+	        <li><a href="#">Điểm Vui Chơi</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Thêm<span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Giới Thiệu </a></li>
+	            <li><a href="#">Giải Đáp</a></li>
+	            <li><a href="#">Hướng Dẫn</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i>Facebook </a></li>
+	            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i>Twitter</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	      
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</div>
+</body>
+</html>
