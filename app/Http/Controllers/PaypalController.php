484bf0e883645f8e2a4e55eb6f0406966503d7e8
Vo Thieu <votienthieu@gmail.com>
2016-11-18 14:48:56 +0700

    test paypal

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Illuminate\Support\Facades\Input;
 use PayPal\Rest\ApiContext;
 use PayPal\Auth\OAuthTokenCredential;
 use PayPal\Api\Amount;
@@ -110,11 +111,11 @@ class PaypalController extends Controller
 	public function getPaymentStatus()
 	{
 	    // Get the payment ID before session clear
-	    $payment_id = Session::get('paypal_payment_id');
+	    $payment_id = Input::get('paymentId'); //\Session::get('paypal_payment_id');
 	    // clear the session payment ID
-	    Session::forget('paypal_payment_id');
+	    \Session::forget('paypal_payment_id');
 	    if (empty(Input::get('PayerID')) || empty(Input::get('token'))) {
-	        return Redirect::route('original.route')
+	        return \Redirect::route('original.route')
 	            ->with('error', 'Payment failed');
 	    }
 
@@ -130,10 +131,10 @@ class PaypalController extends Controller
 	    $result = $payment->execute($execution, $this->_api_context);
 	    echo '<pre>';print_r($result);echo '</pre>';exit; // DEBUG RESULT, remove it later
 	    if ($result->getState() == 'approved') { // payment made
-	        return Redirect::route('original.route')
+	        return \Redirect::route('original.route')
 	            ->with('success', 'Payment success');
 	    }
-	    return Redirect::route('original.route')
+	    return \Redirect::route('original.route')
 	        ->with('error', 'Payment failed');
 	}
 }
